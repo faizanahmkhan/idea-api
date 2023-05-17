35a150e629016268d0985b25729166ae966bfb1e
Faizan Khan <faizan.khan@capgemini.com>
2023-05-17 17:03:55 +0100

    first commit

---
+++ src/main/java/com/example/restapichallenge/RestApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapichallenge/RestApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
